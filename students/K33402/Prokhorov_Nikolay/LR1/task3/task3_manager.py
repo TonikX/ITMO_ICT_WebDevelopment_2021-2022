278fc098b8ed6b1c1e7579b7e75f308f54093226
Nikolay Prokhorov <mail@prokhn.ru>
2021-10-08 11:29:21 +0300

    Done task3

---
+++ students/K33402/Prokhorov_Nikolay/LR1/task3/task3_manager.py
@@ -0,0 +1,31 @@
+import argparse
+import json
+
+from typing import Dict
+import requests
+import tqdm
+
+parser = argparse.ArgumentParser(description='Менеджер для задачи 3')
+parser.add_argument('--host', help='IP хоста', default='localhost')
+parser.add_argument('--port', help='Порт хоста', default=8888)
+
+args = parser.parse_args()
+
+with open('data.json', encoding='utf-8') as file:
+    data: Dict[str, Dict[str, int]] = json.load(file)
+
+for student in data.keys():
+    for discipline in data[student].keys():
+        mark = data[student][discipline]
+        print('\r' + ' ' * 100, end='', flush=True)
+        print(f'\rStudent {student} discipline "{discipline}" mark {mark}', end='', flush=True)
+
+        response = requests.post(f'http://{args.host}:{args.port}/marks', params={
+            'student': student,
+            'discipline': discipline,
+            'mark': mark
+        })
+        if response.status_code == 204:
+            print(' Done!' + ' ' * 30, end='', flush=True)
+        else:
+            print(f' Error for user {student}!')
