cb27343e87d4194adaf7ed66ae16ba3123a9a16d
MatveyVlasov <vlasovmatvey007@gmail.com>
2021-10-19 13:40:16 +0300

    __str__ functions changed

--- students/K33402/Vlasov_Matvey/simple_django_web_project/django_project_Vlasov/project_first_app/models.py
+++ students/K33402/Vlasov_Matvey/simple_django_web_project/django_project_Vlasov/project_first_app/models.py
@@ -6,12 +6,18 @@ class Owner(models.Model):
     last_name = models.CharField(max_length=30)
     birthday = models.DateField()
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class License(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
     number = models.CharField(max_length=10)
     type = models.CharField(max_length=10)
     date = models.DateField()
+
+    def __str__(self):
+        return self.number
 
 
 class Car(models.Model):
@@ -21,9 +27,15 @@ class Car(models.Model):
     color = models.CharField(max_length=30)
     owner = models.ManyToManyField(Owner, through='Ownership')
 
+    def __str__(self):
+        return self.plate
+
 
 class Ownership(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     date_start = models.DateField()
     date_end = models.DateField()
+
+    def __str__(self):
+        return f"{self.owner} | {self.car}"
