2f01811b3c2a7ea684e6246e4373813767080362
MatveyVlasov <vlasovmatvey007@gmail.com>
2021-10-23 13:49:19 +0300

    profile template created

--- students/K33402/Vlasov_Matvey/LR2/homework_board/board/views.py
+++ students/K33402/Vlasov_Matvey/LR2/homework_board/board/views.py
@@ -1,7 +1,21 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+
+from board.models import Discipline, Teacher, Student
 
 
-class Home(TemplateView):
+class HomeView(TemplateView):
     template_name = 'board/home.html'
+
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    login_url = '/login/'
+
+    def get(self, request, *args, **kwargs):
+        teacher = Teacher.objects.all().filter(pk=self.request.user.pk).first()
+        student = Student.objects.all().filter(pk=self.request.user.pk).first()
+        context = {'teacher': teacher, 'student': student}
+        return render(request, 'board/profile.html', context)
