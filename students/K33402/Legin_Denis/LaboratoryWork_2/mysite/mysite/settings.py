bdfd776fdb1e9b9248939038c4fc5bb3617b0dee
aNOOBis666 <mr.zooko@mail.ru>
2021-10-30 05:39:54 +0300

    database models created

--- students/K33402/Legin_Denis/LaboratoryWork_2/mysite/mysite/settings.py
+++ students/K33402/Legin_Denis/LaboratoryWork_2/mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'raceWinners.apps.RaceWinnersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
--- students/K33402/Legin_Denis/LaboratoryWork_2/mysite/raceWinners/models.py
+++ students/K33402/Legin_Denis/LaboratoryWork_2/mysite/raceWinners/models.py
@@ -1,2 +1,33 @@
 from django.db import models
 
+
+class Users(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=50)
+
+
+class Racers(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=50)
+    car = models.CharField(max_length=50)
+
+
+class RaceRacers(models.Model):
+    racer = models.ForeignKey(Racers, on_delete=models.CASCADE)
+    racerRate = models.IntegerField()
+
+
+class Race(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True)
+    dateTime = models.DateTimeField()
+    racers = models.ForeignKey(RaceRacers, on_delete=models.CASCADE)
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    comment_type = models.CharField(max_length=100)
+    comment_text = models.CharField(max_length=500)
+    rate = models.IntegerField()
