22c025cdf01f011bc8594443038fe58135d7b06d
Alena-L <alena.lutskovich@gmail.com>
2021-11-02 21:01:55 +0300

    закончена вторая практика

---
+++ students/K33422/laboratory_works/Луцкович_Алена/LR2/project_first_app/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Owner
+
+class OwnerForm(forms.ModelForm):
+
+    class Meta:
+        model = Owner
+        fields = ['first_name', 'last_name', 'birthday']
--- students/K33422/laboratory_works/Луцкович_Алена/LR2/project_first_app/views.py
+++ students/K33422/laboratory_works/Луцкович_Алена/LR2/project_first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import Http404
-from project_first_app.models import Owner
+from project_first_app.models import Owner, Auto
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
+from .forms import OwnerForm
 
 
 def owner(request, owner_id):
@@ -10,3 +12,38 @@ def owner(request, owner_id):
         raise Http404('Owner does not exist')
 
     return render(request, 'owner.html', {'owner': owner})
+
+def owner_create(request):
+    data = {}
+
+    form = OwnerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    data['form'] = form
+    return render(request, 'project_first_app/owner_create.html', data)
+
+def list_owners(request):
+    owners = {'owners': Owner.objects.all()}
+    return render(request, 'list_owners.html', owners)
+
+class AutoList(ListView):
+    model = Auto
+
+class AutoCreate(CreateView):
+    model = Auto
+    template_name = 'project_first_app/auto_create.html'
+    fields = ['license_plate', 'brand', 'model', 'color']
+    success_url = '/car/list'
+
+class AutoUpdate(UpdateView):
+    model = Auto
+    template_name = 'project_first_app/auto_update.html'
+    fields = ['license_plate', 'color']
+    success_url = '/car/list'
+
+class AutoDelete(DeleteView):
+    model = Auto
+    template_name = 'project_first_app/auto_delete.html'
+    success_url = '/car/list'
+
