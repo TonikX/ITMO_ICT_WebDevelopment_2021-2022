262d03c43468d3874d012835f7ecbef8793db6a6
alexanderlakiza <alelakiza@yandex.ru>
2021-11-08 19:57:22 +0300

    practice is done

---
+++ students/K33421/practical_works/Lakiza_Alexander/django_project_lakiza/project_first_app/form.py
@@ -0,0 +1,16 @@
+from django import forms
+from project_first_app.models import Owner
+
+
+class OwnerForm(forms.ModelForm):
+    class Meta:
+        model = Owner
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "date_of_birth",
+            "passport",
+            "address",
+            "nationality"
+        ]
