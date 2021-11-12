ce3bcc972ea3d895ecd1a248caf054c3cb1b39a5
DoVanThien <vicetone125@gmail.com>
2021-11-12 08:46:17 +0300

    add LR2

---
+++ students/K33401/Do_Thien/Lr2/django_project_lr2/project_lr2_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import *
+
+
+admin.site.register(User)
+admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(Bill)
+admin.site.register(Comment)
