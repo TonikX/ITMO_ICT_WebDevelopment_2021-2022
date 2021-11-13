ee41d79aa2b8cdbea30c8d39617372ac7106774c
aNOOBis666 <mr.zooko@mail.ru>
2021-11-13 03:59:24 +0300

    registrationViews update

--- students/K33402/Legin_Denis/LaboratoryWork_2/mysite/raceWinners/admin.py
+++ students/K33402/Legin_Denis/LaboratoryWork_2/mysite/raceWinners/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Users, Racers, Race, RaceRacers, Comments
 
-# Register your models here.
+admin.site.register(Users)
+admin.site.register(Racers)
+admin.site.register(Race)
+admin.site.register(RaceRacers)
+admin.site.register(Comments)
--- students/K33402/Legin_Denis/LaboratoryWork_2/mysite/raceWinners/models.py
+++ students/K33402/Legin_Denis/LaboratoryWork_2/mysite/raceWinners/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class Users(models.Model):
+    __tablename__ = "users"
     id = models.IntegerField(primary_key=True, auto_created=True)
     username = models.CharField(max_length=25)
     password = models.CharField(max_length=50)
 
 
 class Racers(models.Model):
+    __tablename__ = "racers"
     id = models.IntegerField(primary_key=True, auto_created=True)
     username = models.CharField(max_length=25)
     password = models.CharField(max_length=50)
@@ -15,17 +17,20 @@ class Racers(models.Model):
 
 
 class RaceRacers(models.Model):
+    __tablename__ = "race_racers"
     racer = models.ForeignKey(Racers, on_delete=models.CASCADE)
     racerRate = models.IntegerField()
 
 
 class Race(models.Model):
+    __tablename__ = "race"
     id = models.IntegerField(primary_key=True, auto_created=True)
     dateTime = models.DateTimeField()
     racers = models.ForeignKey(RaceRacers, on_delete=models.CASCADE)
 
 
 class Comments(models.Model):
+    __tablename__ = "comments"
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     date = models.DateTimeField()
     comment_type = models.CharField(max_length=100)
