499a048f10d02d269d71079d1bca3cf073205cb9
dream_shh <shtreyhann@yandex.ru>
2021-11-23 18:58:42 +0300

    изменено 2 задание

--- students/K33401/Shtreikh_Anna/Lr1/2/client.py
+++ students/K33401/Shtreikh_Anna/Lr1/2/client.py
@@ -4,10 +4,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('127.0.0.1', 9900))
 
 a = input("Введите значение верхнего основания трпаеции. ").encode('utf-8')
-b = input("Введите значение нижнего основания трапеции. ").encode('utf-8')
-h = input("Введите значение высоты трапеции. ").encode('utf-8')
 sock.send(a)
+b = input("Введите значение нижнего основания трапеции. ").encode('utf-8')
 sock.send(b)
+h = input("Введите значение высоты трапеции. ").encode('utf-8')
 sock.send(h)
 
 s = sock.recv(1024)
