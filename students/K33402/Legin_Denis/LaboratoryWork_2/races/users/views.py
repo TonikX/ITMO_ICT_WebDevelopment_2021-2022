f6c3b3d5cdf939e8d6b1e8ae4cb584dc8de417cd
aNOOBis666 <mr.zooko@mail.ru>
2021-12-02 15:28:06 +0300

    Lab2 updated

---
+++ students/K33402/Legin_Denis/LaboratoryWork_2/races/users/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            'username', 'password1', 'password2', 'first_name', 'last_name', 'team', 'car_description', 'experience',
+            'driver_class')
+
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
