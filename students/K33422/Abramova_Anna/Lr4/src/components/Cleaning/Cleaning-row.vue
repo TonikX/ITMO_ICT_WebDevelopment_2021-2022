4df1bf5d8d230437edce48b565061c98b988a13e
”Ivan <abramik.01@mail.ru>
2021-12-23 09:42:03 +0300

    добавлена лр 4

---
+++ students/K33422/Abramova_Anna/Lr4/src/components/Cleaning/Cleaning-row.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h3>Уборщик: {{cleaner}}</h3>
+		<p>Этаж: {{cleaning_floor}}</p>
+		<p>Дата уборки: {{cleaning_day}}</p>
+		<a :href="'/cleanings/' + id" class="btn btn-primary">Подробнее</a>
+		<hr>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CleaningRow',
+	props: ['id', 'cleaner', 'cleaning_floor', 'cleaning_day']
+}
+</script>
+
+<style>
+
+</style>
