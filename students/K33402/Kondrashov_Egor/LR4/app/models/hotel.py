9e64f668d6933c8cfc239f9b3ce9805178229eac
Egor Kondrashov <e.kondr01@gmail.com>
2022-01-11 17:05:20 +0300

    feat: add more fields to hotel model

--- students/K33402/Kondrashov_Egor/LR4/app/models/hotel.py
+++ students/K33402/Kondrashov_Egor/LR4/app/models/hotel.py
@@ -8,6 +8,11 @@ class Hotel(models.Model):
     """
 
     name = fields.CharField(max_length=255, unique=True, index=True)
+    address = fields.CharField(max_length=1023, null=True)
+    img_src = fields.CharField(max_length=2047, null=True)
+    description = fields.TextField(null=True)
+    rating = fields.FloatField(null=True)
+    cost_from = fields.FloatField(null=True)
 
 
 HotelSchema = pydantic_model_creator(Hotel)
