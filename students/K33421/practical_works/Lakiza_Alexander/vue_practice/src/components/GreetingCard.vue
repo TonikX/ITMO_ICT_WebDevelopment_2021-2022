78e8663a1871fc8677dca2f48711198eeb5d4c8e
alexanderlakiza <alelakiza@yandex.ru>
2022-01-14 13:56:47 +0300

    practice 4 is done

---
+++ students/K33421/practical_works/Lakiza_Alexander/vue_practice/src/components/GreetingCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Привет<span v-if="username">, {{ username }}</span>!</h1>
+</template>
+
+<script>
+export default {
+  name: 'GreetingCard',
+
+  props: {
+    username: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
