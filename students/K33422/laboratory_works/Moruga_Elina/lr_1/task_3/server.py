d8d7f7be1cb12f32781c0d05ed7bc639625f4d57
Elina Moruga <moruga-elina@mail.ru>
2022-01-17 18:17:33 +0300

    Исправлено задание 1.3

--- students/K33422/laboratory_works/Moruga_Elina/lr_1/task_3/server.py
+++ students/K33422/laboratory_works/Moruga_Elina/lr_1/task_3/server.py
@@ -1,23 +1,25 @@
 import socket
-import sys
+from email.parser import Parser
+from functools import lru_cache
+from urllib.parse import parse_qs, urlparse
 
-MAX_LINE = 64*1024
+MAX_LINE = 64 * 1024
+MAX_HEADERS = 100
+
 
 class MyHTTPServer:
-  # Параметры сервера
-    def __init__(self, host, port, name):
-        self.host = host 
-        self.port = port 
-        self.name = name
-  
+    def __init__(self, host, port, server_name):
+        self._host = host
+        self._port = port
+        self._server_name = server_name
+        self._subjects = {}
+
     def serve_forever(self):
-    # 1. Запуск сервера на сокете, 
-    # обработка входящих соединений
-        serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+        serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
-            serv_sock.bind((self.host, self.port))
-            serv_sock.listen(1)
+            serv_sock.bind((self._host, self._port))
+            serv_sock.listen()
 
             while True:
                 conn, _ = serv_sock.accept()
@@ -29,7 +31,6 @@ class MyHTTPServer:
             serv_sock.close()
 
     def serve_client(self, conn):
-        # 2. Обработка клиентского подключения
         try:
             req = self.parse_request(conn)
             resp = self.handle_request(req)
@@ -40,147 +41,167 @@ class MyHTTPServer:
             self.send_error(conn, e)
 
         if conn:
+            req.rfile.close()
             conn.close()
 
     def parse_request(self, conn):
-        # 3. функция для обработки заголовка http+запроса. 
-        # Python, сокет предоставляет возможность создать 
-        # вокруг него некоторую обертку, которая предоставляет 
-        # file object интерфейс. 
-        # Это дайте возможность построчно обработать запрос. 
-        # Заголовок всегда - первая строка. Первую строку 
-        # нужно разбить на 3 элемента 
-        # (метод + url + версия протокола). 
-        # URL необходимо разбить на адрес и параметры 
-        # (isu.ifmo.ru/pls/apex/f?p=2143 , 
-        # где isu.ifmo.ru/pls/apex/f, 
-        # а p=2143 - параметр p со значением 2143)
         rfile = conn.makefile('rb')
-        raw = rfile.readline(MAX_LINE + 1)  
+        method, target, ver = self.parse_request_line(rfile)
+        headers = self.parse_headers(rfile)
+        host = headers.get('Host')
+        if not host:
+            raise HTTPError(400, 'Bad request', 'Host header is missing')
+        if host not in (self._server_name, f'{self._server_name}:{self._port}'):
+            raise HTTPError(404, 'Not found')
+        return Request(method, target, ver, headers, rfile)
+
+    def parse_request_line(self, rfile):
+        raw = rfile.readline(MAX_LINE + 1)
         if len(raw) > MAX_LINE:
-            raise Exception('Request line is too long')
+            raise HTTPError(400, 'Bad request', 'Request line is too long')
 
         req_line = str(raw, 'iso-8859-1')
-        req_line = req_line.rstrip('\r\n')
-        words = req_line.split()            # разделяем по пробелу
-        if len(words) != 3:                 # и ожидаем ровно 3 части
-            raise Exception('Malformed request line')
+        words = req_line.split()
+        if len(words) != 3:
+            raise HTTPError(400, 'Bad request', 'Malformed request line')
 
-        method, url, ver = words
+        method, target, ver = words
         if ver != 'HTTP/1.1':
-            raise Exception('Unexpected HTTP version')
+            raise HTTPError(505, 'HTTP Version Not Supported')
+        return method, target, ver
 
-        url, params = url.split("?")
-        params = params.split("&")
-        params_dct = {}
-        for i in range(len(params)):
-            # p = {} 
-            p_list = params[i].split("=") 
-            params_dct[p_list[0]] = p_list[1]
-            # params[i] = p 
+    def parse_headers(self, rfile):
+        headers = []
+        while True:
+            line = rfile.readline(MAX_LINE + 1)
+            if len(line) > MAX_LINE:
+                raise HTTPError(494, 'Request header too large')
 
-        dct = {"method": method, "url": url, 
-                "params": params_dct, "ver": ver}
-        # print(dct)
-        return dct
+            if line in (b'\r\n', b'\n', b''):
+                break
 
+            headers.append(line)
+            if len(headers) > MAX_HEADERS:
+                raise HTTPError(494, 'Too many headers')
+
+        sheaders = b''.join(headers).decode('iso-8859-1')
+        return Parser().parsestr(sheaders)
 
     def handle_request(self, req):
-        # 5. Функция для обработки url в соответствии 
-        # с нужным методом. В случае данной работы, 
-        # нужно будет создать набор условий, 
-        # который обрабатывает GET или POST запрос. 
-        # GET запрос должен возвращать данные. 
-        # POST запрос должен записывать данные на основе 
-        # переданных параметров.
-        if req["method"] == "GET":
-            with open("index.html", "rb") as f:
-                data = f.read() 
+        if req.path == '/subjects' and req.method == 'POST':
+            return self.handle_post_subjects(req)
 
-            # print(data.decode())
-            headers = ['Content-Type: text/html\r\n',
-                        f'Content-Length: {len(data)}\r\n']
+        if req.path == '/subjects' and req.method == 'GET':
+            return self.handle_get_subjects(req)
 
-            response = {"code": 200, "reason": "OK", "headers": headers, "body": data}
-            return response
+        raise HTTPError(404, 'Not found')
 
-        if req["method"] == "POST":
-            # print(req["params"]) 
-            params = req["params"]
-            data = ""
-            for param in params:
-                st = f'\n\t\t\t<tr>\n\
-                \t<td>{param}</td>\n\
-                \t<td>{params[param]}</td>\n\
-                </tr>'
-                data += st
-
-            # читаем текущий index.html            
-            with open("index.html", "r") as f:
-                text = f.read() 
-            
-            # находим последнюю запись
-            if "</tr>" in text:
-                ind = text.rindex("</tr>")+len("</tr>")
-                new_text = text[:ind]
-                new_text += data  
-                new_text += text[ind:]
-            
-            # записываем новые данные
-            with open("index.html", "wb") as f:
-                f.write(new_text.encode())
-
-            response = {"code": 204, "reason": "Grade added"}
-
-            return response
-
-
-    def send_response(self, conn, response_data):
-        # 6. Функция для отправки ответа. 
-        # Необходимо записать в соединение 
-        # status line вида 
-        # HTTP/1.1 <status_code> <reason>. 
-        # Затем, построчно записать заголовки 
-        # и пустую строку, 
-        # обозначающую конец секции заголовков.
-        # print(response_data)
+    def send_response(self, conn, resp):
         wfile = conn.makefile('wb')
-        status_line = f"HTTP/1.1 {response_data['code']} {response_data['reason']}\r\n"
+        status_line = f'HTTP/1.1 {resp.status} {resp.reason}\r\n'
         wfile.write(status_line.encode('iso-8859-1'))
-        
-        if "headers" in response_data:
-            for header in response_data["headers"]:
-                wfile.write(header.encode('iso-8859-1'))
-        
+
+        if resp.headers:
+            for (key, value) in resp.headers:
+                header_line = f'{key}: {value}\r\n'
+                wfile.write(header_line.encode('iso-8859-1'))
+
         wfile.write(b'\r\n')
 
-        if "body" in response_data:
-            wfile.write(response_data["body"])
+        if resp.body:
+            wfile.write(resp.body)
 
         wfile.flush()
         wfile.close()
 
-    def send_error(self, conn, e):
-        print("send_error", e)
+    def send_error(self, conn, err):
         try:
-            body = e.encode('utf-8')
+            status = err.status
+            reason = err.reason
+            body = (err.body or err.reason).encode('utf-8')
         except:
             status = 500
             reason = b'Internal Server Error'
             body = b'Internal Server Error'
-            
-            headers = [f'Content-Length: {len(body)}\r\n']
-            resp = {"code": status, "reason": reason, "headers": headers, "body": body}
-
+        resp = Response(status, reason, [('Content-Length', len(body))], body)
         self.send_response(conn, resp)
+
+    def handle_post_subjects(self, req):
+        subject_id = len(self._subjects) + 1
+        self._subjects[subject_id] = {'subject': req.query['subject'][0], 'mark': req.query['mark'][0]}
+        return Response(204, 'Created')
+
+    def handle_get_subjects(self, req):
+        accept = req.headers.get('Accept')
+        if 'text/html' in accept:
+            contentType = 'text/html; charset=utf-8'
+            body = '<html><head></head><body>'
+            body += f'<div>Subjects</div>'
+            body += '<ul>'
+            for u in self._subjects.values():
+                body += f'<li>#{u["subject"]}, {u["mark"]}</li>'
+            body += '</ul>'
+            body += '</body></html>'
+        else:
+            return Response(406, 'Not Acceptable')
+
+        body = body.encode('utf-8')
+        headers = [('Content-Type', contentType), ('Content-Length', len(body))]
+        return Response(200, 'OK', headers, body)
+
+
+class Request:
+    def __init__(self, method, target, version, headers, rfile):
+        self.method = method
+        self.target = target
+        self.version = version
+        self.headers = headers
+        self.rfile = rfile
+
+    @property
+    def path(self):
+        return self.url.path
+
+    @property
+    @lru_cache(maxsize=None)
+    def query(self):
+        return parse_qs(self.url.query)
+
+    @property
+    @lru_cache(maxsize=None)
+    def url(self):
+        return urlparse(self.target)
+
+    def body(self):
+        size = self.headers.get('Content-Length')
+        if not size:
+            return None
+        return self.rfile.read(size)
+
+
+class Response:
+    def __init__(self, status, reason, headers=None, body=None):
+        self.status = status
+        self.reason = reason
+        self.headers = headers
+        self.body = body
+
+
+class HTTPError(Exception):
+    def __init__(self, status, reason, body=None):
+        super()
+        self.status = status
+        self.reason = reason
+        self.body = body
+
 
 
 if __name__ == '__main__':
-    host = "localhost"
-    port = 9090 
-    name = "MyServer"
+    host = "127.0.0.1"
+    port = 8008
+    subject = 'elya'
 
-    serv = MyHTTPServer(host, port, name)
+    serv = MyHTTPServer(host, port, subject)
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
