e2923c1fe8b3cb1df41a58f758dab189db0faa2f
Anna Tostanovskaya <relanwo@gmail.com>
2022-01-19 00:16:59 +0300

    лаба 1

--- students/К33422/Редичкина Анна/LR2/first_project/first_project/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Owner, Car, auto_Owner, ID_owner
-
-admin.site.register(Owner)
-admin.site.register(Car)
-admin.site.register(auto_Owner)
-admin.site.register(ID_owner)
