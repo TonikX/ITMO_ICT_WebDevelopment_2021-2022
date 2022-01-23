4ce2ac9c48dc2acc0a20f2d4d8be4fe29fe71d05
igorlitvak <igr.litvak@gmail.com>
2022-01-23 16:23:31 +0300

    Remove password validation for convenience

--- students/k33401/Litvak_Igor/Lr3/weatherapi/settings.py
+++ students/k33401/Litvak_Igor/Lr3/weatherapi/settings.py
@@ -89,26 +89,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
