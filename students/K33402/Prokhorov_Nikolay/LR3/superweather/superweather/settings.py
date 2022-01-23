81a891d767ce26ecc9a23952c22fe86083abd8f2
Nikolay Prokhorov <mail@prokhn.ru>
2022-01-24 02:54:28 +0300

    Add djoser auth

--- students/K33402/Prokhorov_Nikolay/LR3/superweather/superweather/settings.py
+++ students/K33402/Prokhorov_Nikolay/LR3/superweather/superweather/settings.py
@@ -35,6 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'drf_yasg',
+    'weather'
 ]
 
 MIDDLEWARE = [
--- students/K33402/Prokhorov_Nikolay/LR3/superweather/superweather/urls.py
+++ students/K33402/Prokhorov_Nikolay/LR3/superweather/superweather/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SuperWeather API",
+        default_version='v1'
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
