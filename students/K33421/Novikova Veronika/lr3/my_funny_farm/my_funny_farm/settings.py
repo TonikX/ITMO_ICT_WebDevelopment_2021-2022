9a2bb5fffeb761ba210260e34507a8864004d6f3
aglaya-pill <fedorsov21@mail.ru>
2022-03-09 04:47:22 -0800

    changed LR 1, 2, 3 plus practice

--- students/K33421/Novikova Veronika/lr3/my_funny_farm/my_funny_farm/settings.py
+++ students/K33421/Novikova Veronika/lr3/my_funny_farm/my_funny_farm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g3fluij^0llf@#e^+9cpxnr@s9tkj)kuq(vuy@i8a0!#e+mwkm'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
