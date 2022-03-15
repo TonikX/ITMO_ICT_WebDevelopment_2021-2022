7204834eab60ccba306c994703742c3f0e73555c
Stefania-K <55945055+Stefania-K@users.noreply.github.com>
2022-03-15 13:34:10 +0300

    Delete students/K33421/Khlestunova_Stefania/LR2/library_project directory

--- students/K33421/Khlestunova_Stefania/LR2/library_project/library_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(Book)
-admin.site.register(Readers)
-admin.site.register(Halls)
-admin.site.register(BookAttachment)
-admin.site.register(ReaderAttachment)
