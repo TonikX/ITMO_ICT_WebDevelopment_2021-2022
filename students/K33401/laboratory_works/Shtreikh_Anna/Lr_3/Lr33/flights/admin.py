63ce5a4f31de224a674ff11985b351cdec41028c
dream_shh <shtreyhann@yandex.ru>
2022-03-17 21:39:38 +0300

    idea files deleted

--- students/K33401/laboratory_works/Shtreikh_Anna/Lr_3/Lr33/flights/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from flights.models import *
-
-
-admin.site.register(Flight)
-admin.site.register(Ticket)
-admin.site.register(Plane)
-admin.site.register(Passenger)
-admin.site.register(AirCompany)
