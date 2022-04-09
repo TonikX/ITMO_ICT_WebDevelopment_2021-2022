397580a3576c6df31421a08c41e79540298c38ba
Monyakkk <kom_sasha2001@mail.ru>
2022-04-09 08:30:15 +0300

    added LW2, LW3, LW4 with reports

---
+++ students/K33402/Komissarov_Alexander/LW4/src/views/Author.vue
@@ -0,0 +1,20 @@
+<template>
+    <main class="main-bg">
+        <h1 class="main-h1">
+            Здравствуйте!
+        </h1>
+        <h3 class="base-line">
+            Спасибо, что воспользовались моим сайтом. Меня зовут Александр, мне 20 лет. Я учусь в Санкт-Петербургском Униврситетете ИТМО.
+        </h3>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "Author"
+    }
+</script>
+
+<style scoped>
+
+</style>
